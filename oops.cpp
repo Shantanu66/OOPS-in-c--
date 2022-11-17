587b1361b7a8e2a3847bfbfe98ea05c61b34326e
Maxman <shantanur66@gmail.com>
2022-11-17 23:39:03 +0530

    update

--- oops.cpp
+++ oops.cpp
@@ -340,3 +340,17 @@ public:
         this->name=new char[strlen(s.name)+1];   //deep copy
         strcpy(this->name,s.name);
     }
+    student(int age,char *name)
+    {
+        this->age=age;
+        //this->name=name; shallow copy(avoid this)
+        ///deep copy(create new and copy the previous array
+        this->name=new char[strlen(name)+1];
+        strcpy(this->name,name);
+
+    }
+    void display()
+    {
+        cout<<name<<" "<<age<<endl;
+    }
+};
